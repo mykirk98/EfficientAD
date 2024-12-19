b2fbe259dd8dca450152a896278c05259d59a23c
mykirk98 <mykirk98@gmail.com>
2024-12-19 20:35:47 +0900

    put description on all class and function

--- common.py
+++ common.py
@@ -3,17 +3,38 @@
 from torchvision.datasets import ImageFolder
 
 class ImageFolderWithoutTarget(ImageFolder):
-    def __getitem__(self, index):
-        sample, target = super().__getitem__(index)
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+            
+        Returns:
+            tuple: image
+        """
+        sample, target = super().__getitem__(index=index)
         return sample
 
 class ImageFolderWithPath(ImageFolder):
-    def __getitem__(self, index):
+    def __getitem__(self, index: int):
+        """
+        Args:
+            index (int): Index
+            
+        Returns:
+            sample: image
+            target: target of the image
+            path: path of the image
+        """
         path, target = self.samples[index]
         sample, target = super().__getitem__(index)
+        
         return sample, target, path
 
 def InfiniteDataloader(loader):
+    """
+    Args:
+        loader: DataLoader object
+    """
     iterator = iter(loader)
     while True:
         try:
