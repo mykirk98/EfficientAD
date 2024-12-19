ffbd1ec5fc4f26701621d6909fc1f1c398dc30f8
mykirk98 <mykirk98@gmail.com>
2024-12-19 20:38:20 +0900

    update parser arguments

--- util/parser_.py
+++ util/parser_.py
@@ -8,7 +8,7 @@ def get_argparse():
     
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', dest='dataset', type=str, default='mvtec_ad', choices=['MVTec_AD', 'MVTec_LOCO_AD'])
-    parser.add_argument('--subdataset', dest='subdataset', type=str, help='One of 15 sub-datasets of Mvtec AD or 5 sub-datasets of Mvtec LOCO')
+    parser.add_argument('--category', dest='category', type=str, help='One of 15 sub-datasets of Mvtec AD or 5 sub-datasets of Mvtec LOCO')
     parser.add_argument('--output_dir', dest='output_dir', type=str, default='output/1')
     parser.add_argument('--model_size', dest='model_size', type=str, default='small', choices=['small', 'medium'])
     parser.add_argument('--weights', dest='weights', type=str, default='models/teacher_small.pth')
@@ -18,7 +18,7 @@ def get_argparse():
     args = parser.parse_args()
     
     print(f"dataset : {yellow(args.dataset)}")
-    print(f"subdataset : {magenta(args.subdataset)}")
+    print(f"category : {magenta(args.category)}")
     print(f"output_dir : {yellow(args.output_dir)}")
     print(f"model_size : {magenta(args.model_size)}")
     print(f"weights : {yellow(args.weights)}")
