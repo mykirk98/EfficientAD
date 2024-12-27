75ae7ae4408186bdab394cb184fdd5c880d4bcc0
mykirk98 <mykirk98@gmail.com>
2024-12-27 13:30:36 +0900

    update file structures

--- src/data.py
+++ src/data.py
@@ -1,16 +1,21 @@
 from torchvision import transforms
 from util.hardware import load_json
 
-# file = load(fp='/home/msis/Work/anomalyDetector/EfficientAD/parameters.json')
-# image_size = file['image_size']
-# json_file = load_json(fp='/home/msis/Work/anomalyDetector/EfficientAD/parameters.json')
-# image_size = json_file['image_size']
-
-image_size = load_json(fp='/home/msis/Work/anomalyDetector/EfficientAD/parameters.json')['image_size']
-
+json_file = load_json(fp='/home/msis/Work/anomalyDetector/EfficientAD/parameters.json')
+image_size = json_file['image_size']
+channel_mean = json_file['channel_mean']
+channel_std = json_file['channel_std']
 
 basic_transform = transforms.Compose(transforms=[
                         transforms.Resize(size=(image_size, image_size)),
                         transforms.ToTensor(),
-                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                        transforms.Normalize(mean=channel_mean, std=channel_std)
                     ])
+
+penalty_transform = transforms.Compose(transforms=[
+                        transforms.Resize(size=(2*image_size, 2*image_size)),
+                        transforms.RandomGrayscale(p=0.3),
+                        transforms.CenterCrop(size=(image_size, image_size)),
+                        transforms.ToTensor(),
+                        transforms.Normalize(mean=channel_mean, std=channel_std)
+])
